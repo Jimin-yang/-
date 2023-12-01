a15a114ce2fcf798152ada424d417c515cf0806b
Jimin-yang <jimin010303@gmail.com>
2023-12-01 12:46:09 +0900

    디자인 추가 ,  미완성

--- DB_Project/PT_Project/회원_평점_조회.Designer.cs
+++ DB_Project/PT_Project/회원_평점_조회.Designer.cs
@@ -29,19 +29,50 @@ namespace PT_Project
         /// </summary>
         private void InitializeComponent()
         {
+            this.DBGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // DBGrid
+            // 
+            this.DBGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DBGrid.Location = new System.Drawing.Point(54, 66);
+            this.DBGrid.Margin = new System.Windows.Forms.Padding(2);
+            this.DBGrid.Name = "DBGrid";
+            this.DBGrid.RowHeadersWidth = 62;
+            this.DBGrid.RowTemplate.Height = 30;
+            this.DBGrid.Size = new System.Drawing.Size(222, 316);
+            this.DBGrid.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(317, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "회원 평점 조회";
             // 
             // 회원평점조회
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DBGrid);
             this.Name = "회원평점조회";
             this.Text = "회원_평점_조회";
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DBGrid;
+        private System.Windows.Forms.Label label1;
     }
 }
