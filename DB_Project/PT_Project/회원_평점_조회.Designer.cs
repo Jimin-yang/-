f985f887e120168e320f0eaeb0afbccc476b3a9a
Jimin-yang <jimin010303@gmail.com>
2023-12-11 16:34:18 +0900

    회원 평점조회 폼 완성

--- DB_Project/PT_Project/회원_평점_조회.Designer.cs
+++ DB_Project/PT_Project/회원_평점_조회.Designer.cs
@@ -29,6 +29,7 @@ namespace PT_Project
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream
             this.DBGrid = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).BeginInit();
@@ -44,27 +45,161 @@ namespace PT_Project
             this.DBGrid.RowTemplate.Height = 30;
             this.DBGrid.Size = new System.Drawing.Size(222, 316);
             this.DBGrid.TabIndex = 4;
+=======
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DAOpen = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RatingAverage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Rating = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comment = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(74, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(225, 326);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // DAOpen
+            // 
+            this.DAOpen.Location = new System.Drawing.Point(145, 406);
+            this.DAOpen.Name = "DAOpen";
+            this.DAOpen.Size = new System.Drawing.Size(75, 23);
+            this.DAOpen.TabIndex = 1;
+            this.DAOpen.Text = "조회";
+            this.DAOpen.UseVisualStyleBackColor = true;
+            this.DAOpen.Click += new System.EventHandler(this.DAOpen_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.RatingAverage);
+            this.groupBox1.Location = new System.Drawing.Point(388, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "평균 평점";
+            // 
+            // RatingAverage
+            // 
+            this.RatingAverage.Location = new System.Drawing.Point(34, 41);
+            this.RatingAverage.Name = "RatingAverage";
+            this.RatingAverage.Size = new System.Drawing.Size(100, 21);
+            this.RatingAverage.TabIndex = 0;
+>>>>>>> Stashed changes
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+<<<<<<< Updated upstream
             this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.Location = new System.Drawing.Point(317, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(114, 16);
             this.label1.TabIndex = 5;
             this.label1.Text = "회원 평점 조회";
+=======
+            this.label1.Location = new System.Drawing.Point(140, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "점";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comment);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.Rating);
+            this.groupBox2.Location = new System.Drawing.Point(388, 165);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(315, 201);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "선택 항목";
+            // 
+            // Rating
+            // 
+            this.Rating.Location = new System.Drawing.Point(34, 34);
+            this.Rating.Name = "Rating";
+            this.Rating.Size = new System.Drawing.Size(100, 21);
+            this.Rating.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "점";
+            // 
+            // comment
+            // 
+            this.comment.Location = new System.Drawing.Point(34, 101);
+            this.comment.Multiline = true;
+            this.comment.Name = "comment";
+            this.comment.Size = new System.Drawing.Size(257, 85);
+            this.comment.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(133, 22);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(51, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "날짜 검색";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+>>>>>>> Stashed changes
             // 
             // 회원평점조회
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+<<<<<<< Updated upstream
             this.Controls.Add(this.label1);
             this.Controls.Add(this.DBGrid);
             this.Name = "회원평점조회";
             this.Text = "회원_평점_조회";
             ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).EndInit();
+=======
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.DAOpen);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "회원평점조회";
+            this.Text = "회원_평점_조회";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+>>>>>>> Stashed changes
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,7 +207,21 @@ namespace PT_Project
 
         #endregion
 
+<<<<<<< Updated upstream
         private System.Windows.Forms.DataGridView DBGrid;
         private System.Windows.Forms.Label label1;
+=======
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button DAOpen;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox RatingAverage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox comment;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Rating;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+>>>>>>> Stashed changes
     }
 }
--- DB_Project/PT_Project/회원_평점_조회.cs
+++ DB_Project/PT_Project/회원_평점_조회.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +19,93 @@ namespace PT_Project
         {
             InitializeComponent();
             SelectID = select;
+        }
+
+        private void DAOpen_Click(object sender, EventArgs e)
+        {
+            // TODO: 데이터베이스 연결 문자열을 수정해주세요.
+            string connectionString = "User Id = ptadmin; Password = 1111; Data Source = (DESCRIPTION = (ADDRESS =(PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = xe))); ";
+
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                connection.Open();
+
+                // TODO: SQL 쿼리를 수정해주세요.
+                string query = $"SELECT T_Name as 트레이너, P_date as 작성날짜, P_Grade, P_Feedback FROM consumerDiet " +
+                               "INNER JOIN program ON consumerDiet.P_NO = program.P_NO " +
+                               "INNER JOIN trainer ON program.T_NO = trainer.T_NO " +
+                               $"WHERE program.U_NO = {SelectID}";
+
+                // 날짜 선택 체크박스가 체크되어 있으면 선택한 날짜의 데이터만 가져오도록 쿼리를 수정
+                if (checkBox1.Checked)
+                {
+                    string selectedDate = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+                    query += $" AND TO_CHAR(P_date, 'YYYY-MM-DD') = '{selectedDate}'";
+                }
+
+                using (OracleCommand command = new OracleCommand(query, connection))
+                {
+                    OracleDataAdapter adapter = new OracleDataAdapter(command);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    // 필요한 열만 선택
+                    DataTable filteredTable = new DataTable();
+                    filteredTable.Columns.Add("트레이너", typeof(string));
+                    filteredTable.Columns.Add("작성날짜", typeof(DateTime));
+                    filteredTable.Columns.Add("P_Grade", typeof(int));
+                    filteredTable.Columns.Add("P_Feedback", typeof(string));
+
+                    foreach (DataRow row in table.Rows)
+                    {
+                        filteredTable.Rows.Add(row["트레이너"], row["작성날짜"], row["P_Grade"], row["P_Feedback"]);
+                    }
+
+                    dataGridView1.DataSource = filteredTable;
+
+                    // TODO: 로그인한 회원의 평균 평점을 계산하고 출력하세요.
+                    double averageRating = CalculateAverageRating(table);
+                    RatingAverage.Text = $"평균 평점: {averageRating:F2}";
+                }
+            }
+        }
+
+        private double CalculateAverageRating(DataTable table)
+        {
+            if (table.Rows.Count == 0)
+                return 0;
+
+            double totalRating = 0;
+
+            foreach (DataRow row in table.Rows)
+            {
+                if (row["P_Grade"] != DBNull.Value)
+                    totalRating += Convert.ToDouble(row["P_Grade"]);
+            }
+
+            return totalRating / table.Rows.Count;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridView1.Rows.Count)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+
+                // 원래의 열 이름인 "P_Grade"와 "P_Feedback"을 사용
+                Rating.Text = selectedRow.Cells["P_Grade"].Value.ToString();
+                comment.Text = selectedRow.Cells["P_Feedback"].Value.ToString();
+            }
+            else
+            {
+                Rating.Text = string.Empty;
+                comment.Text = string.Empty;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Enabled = checkBox1.Checked;
         }
     }
 }
